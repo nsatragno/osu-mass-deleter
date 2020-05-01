4825ef8d95bd699a213040285a64105520b417bd
Nina Satragno <nsatragno@gmail.com>
2020-05-01 18:47:56 -0400

    Add checkbox to remove empty folders.

--- src/ar/com/satragno/osufileeditor/Main.java
+++ src/ar/com/satragno/osufileeditor/Main.java
@@ -8,8 +8,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,14 +22,17 @@ import javax.swing.JTextArea;
 public class Main {
 	private JFrame frame;
 	private JLabel filenameLabel;
-	private File folder;
 	private JButton previewButton;
 	private JTextArea preview;
+	private JButton deleteButton;
+	private JCheckBox deleteFoldersCheckBox;
+	private Box bottomRow;
+
+	private File folder;
 	private HashSet<File> filesToSave;
 	private HashSet<File> filesToDelete;
-	private JButton deleteButton;
 	private int filesProcessed;
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -51,35 +57,49 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
+		Box mainBox = new Box(BoxLayout.Y_AXIS);
 		JLabel label = new JLabel("BACKUP YOUR DATA BEFORE RUNNING THIS");
-		frame.getContentPane().add(label);
+		mainBox.add(label);
 
 		JLabel label2 = new JLabel("This will delete all data for which mode does not equal 1");
-		frame.getContentPane().add(label2);
+		mainBox.add(label2);
 
 		JLabel label3 = new JLabel("FOLDER:");
-		frame.getContentPane().add(label3);
+		mainBox.add(label3);
 		filenameLabel = new JLabel("No folder selected.");
-		frame.getContentPane().add(filenameLabel);
+		mainBox.add(filenameLabel);
 
 		JButton fileButton = new JButton("Select folder...");
 		fileButton.addActionListener(event -> showFileChooserDialog());
-		frame.getContentPane().add(fileButton);
+		mainBox.add(fileButton);
 
 		previewButton = new JButton("Preview files to be deleted");
 		previewButton.setVisible(false);
 		previewButton.addActionListener(event -> previewDeleteFiles());
-		frame.getContentPane().add(previewButton);
+		mainBox.add(previewButton);
 
 		preview = new JTextArea();
 		preview.setVisible(false);
 		JScrollPane scroll = new JScrollPane(preview);
-		frame.getContentPane().add(scroll);
-		
+		mainBox.add(scroll);
+
+		frame.getContentPane().add(mainBox);
+
+		bottomRow = new Box(BoxLayout.X_AXIS);
+		bottomRow.setVisible(false);
+		bottomRow.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+		deleteFoldersCheckBox = new JCheckBox();
+		deleteFoldersCheckBox.setText("Delete empty folders");
+		bottomRow.add(deleteFoldersCheckBox);
+
+		bottomRow.add(Box.createHorizontalGlue());
+
 		deleteButton = new JButton("DELETE FILES!!!!1uno");
-		deleteButton.setVisible(false);
 		deleteButton.addActionListener(event -> deleteFiles());
-		frame.getContentPane().add(deleteButton);
+		bottomRow.add(deleteButton);
+
+		frame.getContentPane().add(bottomRow);
 	}
 
 	private void showFileChooserDialog() {
@@ -101,11 +121,11 @@ public class Main {
 			markFiles(folder);
 
 			filesToDelete.removeAll(filesToSave);
-			
+
 			ArrayList<File> ordered = new ArrayList<>(filesToDelete.size());
 			filesToDelete.forEach(file -> ordered.add(file));
 			ordered.sort((f1, f2) -> f1.toString().compareTo(f2.toString()));
-			
+
 			StringBuilder text = new StringBuilder();
 			ordered.forEach(file -> text.append(file.getAbsolutePath() + "\n"));
 			preview.setText(text.toString());
@@ -132,10 +152,25 @@ public class Main {
 				throw new RuntimeException(e);
 			}
 		}
-		
-		deleteButton.setVisible(true);
+
+		bottomRow.setVisible(true);
 	}
-	
+
+	private void deleteEmptyFolders(File folder, StringBuilder text) {
+		if (!folder.isDirectory())
+			return;
+		for (File child : folder.listFiles(file -> file.isDirectory())) {
+			deleteEmptyFolders(child, text);
+		}
+		if (folder.listFiles().length == 0) {
+			if (folder.delete()) {
+				text.append("DELETED EMPTY FOLDER: " + folder + "\n");
+			} else {
+				text.append("ERROR DELETING EMPTY FOLDER: " + folder + "\n");
+			}
+		}
+	}
+
 	private void deleteFiles() {
 		StringBuilder text = new StringBuilder();
 		filesToDelete.forEach(file -> {
@@ -145,6 +180,11 @@ public class Main {
 				text.append("ERROR DELETING: " + file + "\n");
 			}
 		});
+
+		if (deleteFoldersCheckBox.isSelected()) {
+			deleteEmptyFolders(folder, text);
+		}
+
 		text.append("Done.\n");
 		preview.setText(text.toString());
 	}
